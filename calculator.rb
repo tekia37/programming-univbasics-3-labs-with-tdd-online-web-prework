82eb4edfd5017903a015d35622a27ba9261596cb
TEKIA     C <tekia37@yahoo.com>
2019-08-31 23:48:14 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -6,7 +6,12 @@ if first_number = Integer
    puts "The first number is a float"
  end
  
- if second_number = Integer 
+ 
+ second_number = 0.0032
+ 
+ if second_number = Integer && != 0
    puts "The second number is an integer."
- else
-  
+ else second_number = Float && != 0
+   puts "The second number is a float."
+end
+
